fc46d0d30d16855dbc00e952a912774517e67749
davidtorres99 <davidntorres1999@gmail.com>
2021-07-24 18:14:56 -0300

    Cambios en menu hamburguesa

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/* Despliegue de menú hamburguesa */
+const barraNavegacion = document.querySelector("#menu-navegacion");
+const menuHamburguesa = document.querySelector("#menu-hamburguesa");
+const btnSobreNosotras = document.querySelector("#btn-sobrenosotras")
+
+menuHamburguesa.addEventListener("click",function(){
+    barraNavegacion.classList.toggle('menu-visible');
+});
+
+btnSobreNosotras.addEventListener("click",function(){
+    barraNavegacion.classList.toggle('menu-visible');
+});
